f96a7fd33f58eac8bf0757996adc153862bc630f
zz_zigzag <zz_zigzag@163.com>
2017-12-17 00:20:10 +0800

    2017-12-17

--- 898A.c
+++ 898A.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define MIN(a, b) ((a)<(b)?(a):(b))
 #define MAX(a, b) ((a)>(b)?(a):(b))
 
 #define MOD 1e9+7
-#define M 5001
+#define M 10000001
 
-typedef enum {false=0, true=1} bool;
-typedef long long ll;
 
 int main() {
     int a;
     scanf("%d", &a);
-    printf("%d\n", a/10*10);
+    printf("%d\n", (a+5)/10*10);
     return 0;
 }
